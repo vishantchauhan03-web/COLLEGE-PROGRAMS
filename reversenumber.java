0582ace15dbe2b3e91cb2ff93b418b402024aa21
vishantchauhan03-web <vishantchauhan03@gmail.com>
2026-08-25 22:09:32 +0530

    Add files via upload

---
+++ reversenumber.java
@@ -0,0 +1,19 @@
+class reversenumber{
+    public static void main(String a[]){
+        int i,j;
+        for(i=1;i<=6;i++){
+            for(j=1;j<=i;j++){
+                System.out.print(j + " ");
+
+            }
+            System.out.println();
+
+        }
+        for(i=5;i>=1;i--){
+            for(j=1;j<=i;j++){
+                System.out.print(j + " ");
+
+            }
+            System.out.println();
+    }
+}}
