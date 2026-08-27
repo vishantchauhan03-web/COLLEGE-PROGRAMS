1be53b2603b02e4d5e6648fcc248402d2b4fb0f0
vishantchauhan03-web <vishantchauhan03@gmail.com>
2026-08-27 19:24:05 +0530

    different type of patterns

---
+++ Practise.java
@@ -0,0 +1,30 @@
+//class Practise{
+  //  public static void main(String arr[]){               54321
+    //    for(int i =1;i<=5;i++){                          4321
+      //      for(int j=5;j>=i;j--){                       321
+        //        System.out.print(j);                     21
+          //  }System.out.println();                       1
+       // }
+    //}
+//}
+
+//class Practise{
+//     public static void main(String arr[]){            1
+//         for(int i=1;i<=5;i++){                        22 
+//             for(int j=1;j<=i;j++){                    333  
+//                 System.out.print(i);                  4444
+//             }System.out.println();
+
+//         }
+        
+//     }
+// }
+// class Practise{                                    1
+//     public static void main(String arr[]){         21
+//         for(int i=1;i<=5;i++){                     321
+//             for(int j=i;j>=1;j--){                 4321
+//                 System.out.print(j);               54321
+//             }System.out.println();
+//         }
+//     }
+// }
