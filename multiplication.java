055e7fd2fb046ae181fcbe6e41add1f3b4a36d78
vishantchauhan03-web <vishantchauhan03@gmail.com>
2026-08-27 18:11:27 +0530

    Add files via upload

---
+++ multiplication.java
@@ -0,0 +1,9 @@
+class multiplication{
+    public static void main(String arr[]){
+        for(int i=1;i<=5;i++){
+            for(int j=1;j<=10;j++){
+                System.out.print(i*j +"\t");
+            }System.out.println();
+        }
+    }
+}
