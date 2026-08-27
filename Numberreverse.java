92226183301f1919d4d8402d8cf3f707704b273d
vishantchauhan03-web <vishantchauhan03@gmail.com>
2026-08-27 18:27:29 +0530

    1

---
+++ Numberreverse.java
@@ -0,0 +1,14 @@
+class Numberreverse{
+    public static void main(String arr[]){
+        for (int i =1;i<=10;i++){
+            for(int j =1;j<=i;j++){
+                System.out.print(j);
+            } System.out.println();
+       }
+       for(int k=9;k>=1;k--){
+        for(int l=1;l<=k;l++){
+            System.out.print(l);
+        }System.out.println();
+       }
+    }
+}
