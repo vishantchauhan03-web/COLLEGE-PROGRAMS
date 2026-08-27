e0a5a7228733f501a42ed701cc29c68218646e3e
vishantchauhan03-web <vishantchauhan03@gmail.com>
2026-08-27 18:11:59 +0530

    Add files via upload

---
+++ Number.java
@@ -0,0 +1,10 @@
+class Number{
+    public static void main(String arr[]){
+        for(int i=1;i<=10;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(j);
+
+            }System.out.println();
+        }
+    }
+}
